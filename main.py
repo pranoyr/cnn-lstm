ea471665f031ccc8d3204b69148823a1ece6fb72
Pranoy R <pranoyalkr@gmail.com>
2020-08-11 00:05:52 +0530

    updated

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
 	# optimizer
 	crnn_params = list(encoder_cnn.parameters()) + \
 		list(decoder_rnn.parameters())
-	optimizer = torch.optim.Adam(crnn_params, lr=opt.lr_rate)
+	optimizer = torch.optim.Adam(crnn_params)
 
 	scheduler = lr_scheduler.ReduceLROnPlateau(
 		optimizer, 'min', patience=opt.lr_patience)
