a90c553535f323e83397d15aa325f1e8be77ef20
Pranoy R <pranoyalkr@gmail.com>
2020-08-11 20:57:35 +0530

    updated

--- utils/generate_data.sh
+++ utils/generate_data.sh
@@ -3,7 +3,7 @@ mkdir ./data/annotation
 rm -rf ./data/image_data
 mkdir ./data/image_data
 
-python3 video_jpg_ucf101_hmdb51.py
-python3 n_frames_ucf101_hmdb51.py
-python3 gen_anns_list.py
-python3 ucf101_json.py
+python3 ./video_jpg_ucf101_hmdb51.py
+python3 ./n_frames_ucf101_hmdb51.py
+python3 ./gen_anns_list.py
+python3 ./ucf101_json.py
