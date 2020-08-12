accc311875dc5380926ee1744db3a3ff3ade3804
Pranoy R <pranoyalkr@gmail.com>
2020-08-12 14:46:44 +0530

    updated

--- models/cnnlstm_attention.py
+++ models/cnnlstm_attention.py
@@ -8,41 +8,24 @@ from torchvision.models import resnet18, resnet101
 
 class EncoderAttnCNN(nn.Module):
     def __init__(self):
-        """Load the pretrained ResNet-152 and replace top fc layer."""
         super(EncoderAttnCNN, self).__init__()
-        # resnet = models.resnet101(pretrained=True)
-        # modules = list(resnet.children())[:-1]      # delete the last fc layer.
-        # self.resnet = nn.Sequential(*modules)
         self.resnet = resnet101(pretrained=True)
         self.resnet.fc = nn.Sequential(nn.Linear(self.resnet.fc.in_features, 300))
-        # self.fc1 = nn.Linear(resnet.fc.in_features, 300)
-        # self.bn1 = nn.BatchNorm1d(512, momentum=0.01)
-        # self.fc2 = nn.Linear(512, 512)
-        # self.bn2 = nn.BatchNorm1d(512, momentum=0.01)
-        # self.fc3 = nn.Linear(512, 300)
-
+        self.attn = nn.Linear(300, 300)
+        
     def forward(self, x_3d):
         cnn_embed_seq = []
         for t in range(x_3d.size(1)):
-            # ResNet CNN
             # with torch.no_grad():
-            x = self.resnet(x_3d[:, t, :, :, :])  # ResNet
-            x = x.view(x.size(0), -1)             # flatten output of conv
+            x = self.resnet(x_3d[:, t, :, :, :]) 
+            x = x.view(x.size(0), -1) 
 
-            # FC layers
-            # x = self.fc1(x)
-            # x = F.relu(x)
-            # x = self.fc2(x)
-            # x = F.relu(x)
-            # x = F.dropout(x, p=0.2)
-            # x = self.fc3(x)
+            attn_weights = F.sigmoid(self.attn(x))  
+            attn_applied = attn_weights * x      
+            cnn_embed_seq.append(attn_applied)
 
-            cnn_embed_seq.append(x)
-
-        # swap time and sample dim such that (sample dim, time dim, CNN latent dim)
+       
         cnn_embed_seq = torch.stack(cnn_embed_seq, dim=0)
-        # cnn_embed_seq: shape=(time_step, batch, input_size)
-
         return cnn_embed_seq
 
 
@@ -55,22 +38,15 @@ class DecoderRNN(nn.Module):
             input_size=300,
             hidden_size=256,
             num_layers=3,
-            # input & output will has batch size as 1s dimension. e.g. (time_step, batch, input_size)
         )
 
         self.fc1 = nn.Linear(256, 128)
         self.fc2 = nn.Linear(128, self.num_classes)
 
     def forward(self, x):
-        """ h_n shape (n_layers, batch, hidden_size), h_c shape (n_layers, batch, hidden_size)
-            None represents zero initial hidden state. RNN_out has shape=(time_step, batch, output_size)
-        """
         self.LSTM.flatten_parameters()
         out, (h_n, h_c) = self.LSTM(x)
-        # FC layers
-        # choose RNN_out at the last time step
         x = self.fc1(out[-1, :, :])
         x = F.relu(x)
-        #x = F.dropout(x, p=0.3)
         x = self.fc2(x)
         return x
