aeb0e93343f891d3f67e248cca1e1f3dfb565b5c
Pranoy R <pranoyalkr@gmail.com>
2020-08-13 18:06:33 +0530

    num_classes updated

--- model.py
+++ model.py
@@ -5,12 +5,9 @@ from models import cnnlstm, cnnlstm_attention
 
 def generate_model(opt, device):
 	assert opt.model in [
-		'cnnlstm', 'cnnlstm_attn'
+		'cnnlstm'
 	]
 
 	if opt.model == 'cnnlstm':
 		encoder, decoder = cnnlstm.EncoderCNN(), cnnlstm.DecoderRNN(num_classes=opt.n_classes)
-	elif opt.model == 'cnnlstm_attn':
-		encoder, decoder = cnnlstm_attention.EncoderAttnCNN(), cnnlstm_attention.DecoderRNN(num_classes=opt.n_classes)
-
 	return encoder.to(device), decoder.to(device)
