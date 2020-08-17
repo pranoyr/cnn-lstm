3ed89f20fbe57488d276d0968757afcf203cd720
Pranoy R <pranoyalkr@gmail.com>
2020-08-17 20:20:28 +0530

    delay added for alerts

--- models/cnnlstm.py
+++ models/cnnlstm.py
@@ -27,7 +27,7 @@ class EncoderCNN(nn.Module):
             # ResNet CNN
             with torch.no_grad():
                 x = self.resnet(x_3d[:, t, :, :, :])  # ResNet
-                x = x.view(x.size(0), -1)             # flatten output of conv
+                # x = x.view(x.size(0), -1)             # flatten output of conv
 
             # FC layers
             # x = self.fc1(x)
